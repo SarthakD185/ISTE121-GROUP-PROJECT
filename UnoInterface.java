74051e0c1587c2bb2022bece074cb62c0010687c
Michael Pacholarz <pacholarzswims@gmail.com>
2022-04-13 13:56:35 -0400

    Add files via upload

---
+++ UnoInterface.java
@@ -0,0 +1,40 @@
+import java.io.Serializable;
+
+public class UnoInterface implements Serializable{
+
+   public String Name;
+   int PlayerNumber;
+
+   //Constructor
+   public UnoInterface(String N){
+
+      Name = N;
+      PlayerNumber = PN;
+
+   }
+
+   //Accessors
+   public String getName(){
+      return Name;
+   }
+   
+   public String getPlayerNumber(){
+      return PlayerNumber;
+   }
+
+   //Mutators
+   public void setName(String x){
+      Name = x;
+   }
+   
+   public void setPlayerNumber(int x){
+      PlayerNumber = x;
+   }
+
+   public String toString(){
+
+      String output = "Player info: \nName: " + Name + "\nPlayer Number: " + PlayerNumber;
+      return output;
+   }
+
+}

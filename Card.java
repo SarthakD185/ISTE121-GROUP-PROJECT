aeecd5523dca6d1afc5e6ea7703ef9d4fbc06c6f
Michael Pacholarz <pacholarzswims@gmail.com>
2022-04-13 14:06:19 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,37 @@
+public class Card {
+   
+   String Color;
+   int Number;
+   
+   //Constructor
+   public Card(String C, int N){
+
+      Color = C;
+      Number = N;
+
+   }
+   
+   //Accessors
+   public String getColor(){
+      return Color;
+   }
+   
+   public int getNumber(){
+      return Number;
+   }
+   
+   //Mutators
+   public void setColor(String x){
+      Color = x;
+   }
+   
+   public void setNumber(int x){
+      Number = x;
+   }
+   
+   public String toString(){
+
+      String output = "Card info: Color: " + Color + "Number: " + Number;
+      return output;
+   }
+}
